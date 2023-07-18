ec175f0ddc79446aacacfb991ff046c3eeb5a33e
nancyliqn91 <nancyliqn91@gmail.com>
2023-07-18 14:34:09 -0700

    add readme.md

--- Blog.js
+++ Blog.js
@@ -2,6 +2,7 @@ import React, { useEffect, useReducer} from 'react';
 import blogReducer from './reducers/blog-reducer'
 import { getReviewsFailure, getReviewsSuccess } from './actions/index';
 import { View, Text, ActivityIndicator, FlatList } from 'react-native';
+// Do not use 
 
 const initialState = {
   isLoaded: false,
@@ -13,7 +14,7 @@ function Blog() {
   const [state, dispatch] = useReducer(blogReducer, initialState);
 
 useEffect(() => {
-  fetch(`https://localhost:5001/api/v1/reviews/`)
+  fetch(`https://localhost:5001/api/v1/travels/`)
     .then(response => {
       if (!response.ok) {
         throw new Error(`${response.status}: ${response.statusText}`);
@@ -22,7 +23,8 @@ useEffect(() => {
       }
     })
     .then((jsonifiedResponse) => {
-      const action = getReviewsSuccess(jsonifiedResponse)
+      // depends on the response. Because the travels are stored in a property of the response called travels, so update the reviews state to be jsonifiedResponse.travels
+      const action = getReviewsSuccess(jsonifiedResponse.travels)
       dispatch(action);
       })
     .catch((error) => {
@@ -47,7 +49,12 @@ useEffect(() => {
         renderItem={({ item }) => (
           <View>
             <Text>Destination: {item.destination}</Text>
+            <Text>City: {item.city}</Text>
             <Text>Country: {item.country}</Text>
+            <Text>Review: {item.review}</Text>
+            <Text>Rating: {item.rating}</Text>
+            <Text>Date: {item.date}</Text>
+            
           </View>
         )}
       />
---
+++ README.md
@@ -0,0 +1,37 @@
+#  Awesome Project
+
+#### By Qian Li  Joe  and Sam Adams😊
+
+#### An Practice React Native project which use a C# backend API and a React Native front end to see the travel reviews world wide on your mobile device. 
+
+## 🌼 Technologies Used
+
+* _React_
+* _React Native_
+* _JSX_
+* _Node.js_
+* _javascript_
+* _Html_
+* _CSS_
+* _Bootstrap_
+* VS Code
+
+## 🌺 Description
+
+* A user can see a list of  available reviews for different countries from the C# API from the ios mobile.
+
+## 🏵️ Setup/Installation Requirements
+
+* Go to `https://github.com/nancyliqn91/Awesome-Project` to clone the GitHub repository.
+* Go to `https://github.com/nancyliqn91/TravelApi` and follow instructions to start the `API` website on your server.
+* In your terminal/command line, go into the folder that was cloned and run `npm install`, this will downloading all the package dependencies.
+* Next run `npm run web` to start the server. Once the server loads, the webpage should load automatically but if not type in the localhost route that your terminal shows like `http://localhost:19006/`.
+
+## 🌷 Known Bugs
+
+* No known bugs as 07/18/2023
+* Please reach out with any found bugs, questions, or concerns to nancyliqn91@gmail.com
+
+## 🌹 License
+[MIT](license.txt)
+Copyright (c) 2023 Qian Li, Joe Wahbeh, Sam Adams
